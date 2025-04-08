6a1decf19c4118201f3c9bca2f956b9e60c98484
Delta Student <deepaks9631342845@gmail.com>
2025-04-08 11:12:15 +0530

    concepts around next

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,20 @@
+// export default function Docs(
+//   {params,}: Promise <{slug: string[]}>;
+// }){
+//   const slug = await params.slug;
+//  if (slug?.length ===2) {
+//     return (
+      
+//         <p>This is the documentation for {slug.join('/')}</p>
+      
+//   );
+// } else if (slug?.length === 1) {
+//     return  <h1>  </h1>
+// ) {
+//   return (
+//     <div>
+//       <h1>Welcome to the Docs page!</h1>
+//       <p>This is the documentation section of our application.</p>      
+//     </div>
+//   );
+// }
